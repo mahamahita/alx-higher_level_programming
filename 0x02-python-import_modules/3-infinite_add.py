bdb95d9973d79cf59840d82e520ee2c0350ba634
MAHA EL OUARDI <maha600019@gmail.com>
2023-09-07 03:34:43 -0700

    yy

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,9 +1,18 @@
 #!/usr/bin/python3
 
 if __name__ == "__main__":
- import sys
- sum=0
-  for arg in sys.argv:
-        if arg != sys.argv[0]:
-            sum += int(arg)
-  print(sum)
+  import sys
+  def inf_add(argv):
+    n = len(argv) - 1
+    if n == 0:
+        print("{:d}".format(n))
+        return
+    else:
+        i = 1
+        add = 0
+        while i <= n:
+            add += int(argv[i])
+            i += 1
+        print("{:d}".format(add))
+
+   add_arg(sys.argv)

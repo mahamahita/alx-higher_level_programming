416802820887b0b654e55d5434bb99800770894f
MAHA EL OUARDI <maha600019@gmail.com>
2023-09-08 06:08:04 -0700

    tt

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,15 +1,16 @@
 #!/usr/bin/python3
-def arg(argv):
-    n=len(argv)-1
-    if n == 0:
-      print("{:d} arguments.".format(n))
-    elif n == 1:
-      print("{:d} argument:".format(n)
-    else:
-      print("{:d} arguments:".format(n))
 
-    if n>=1:
-      i = 1
+def print_arg(argv):
+    n = len(argv) - 1
+    if n == 0:
+        print("{:d} arguments.".format(n))
+    elif n == 1:
+        print("{:d} argument:".format(n))
+    else:
+        print("{:d} arguments:".format(n))
+
+    if n >= 1:
+        i = 1
         while i <= n:
             print("{:d}: {:s}".format(i, argv[i]))
             i += 1
@@ -17,4 +18,5 @@ def arg(argv):
 if __name__ == "__main__":
     import sys
     print_arg(sys.argv)
+)
       
--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,18 +1,18 @@
 #!/usr/bin/python3
+import sys
 
 if __name__ == "__main__":
-  import sys
-  def inf_add(argv):
-    n = len(argv) - 1
-    if n == 0:
-        print("{:d}".format(n))
-        return
-    else:
-        i = 1
-        add = 0
+
+    def inf_add(argv):
+        n = len(argv) - 1
+        if n == 0:
+            print("{:d}".format(n))
+            return
+        else:
+            i = 1
+            add = 0
         while i <= n:
             add += int(argv[i])
             i += 1
-        print("{:d}".format(add))
-
-   add_arg(sys.argv)
+            print("{:d}".format(add))
+    inf_add(sys.argv)

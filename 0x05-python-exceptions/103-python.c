6d7a19c2ecb95a6a3494278549b8c03e71eb0885
MAHA EL OUARDI <maha600019@gmail.com>
2023-09-25 06:45:17 -0700

    exceptins

---
+++ 0x05-python-exceptions/103-python.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * print_python_bytes - print - basic info about Python bytes objects
+ * @p: python object
+ * Return: -
+ **/
+void print_python_bytes(PyObject *p)
+{
+}
+/**
+ * print_python_float - print Python float
+ * @p: python float
+ * Return: -
+ **/
+void print_python_float(PyObject *p)
+{
+}
+/**
+ * print_python_list - print - basic info about Python lists
+ * @p: python object
+ * Return: -
+ **/
+void print_python_list(PyObject *p)
+{
+}
+

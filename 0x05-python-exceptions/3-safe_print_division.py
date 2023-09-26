e19bfcc319cd1f1adb0c4863584f2c9581419c14
MAHA EL OUARDI <maha600019@gmail.com>
2023-09-26 03:11:12 -0700

    tt

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -3,12 +3,8 @@ def safe_print_division(a, b):
     try:
         result = a / b
         print("Inside result: {:.1f}".format(result))
-    except ZeroDivisionError:
-        result = None
-        print("Division by zero! Inside result: {}".format(result))
     except Exception as e:
         result = None
         print("An error occurred: {}. Inside result: {}".format(e, result))
     finally:
         return result
-

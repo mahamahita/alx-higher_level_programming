13ce7d9d48fd9ca157685cba812ffb8385cd6c80
MAHA EL OUARDI <maha600019@gmail.com>
2023-10-27 02:22:25 -0700

    tests3

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -1,9 +1,30 @@
 #!/usr/bin/python3
-"""This module inherits from the list class"""
 
+"""This module defines a custom list class called MyList."""
 
 class MyList(list):
-    """A class that inherits from list"""
+    """A class that inherits from the list class.
+
+    This class provides additional methods and functionality on top of the
+    standard list class.
+
+    Attributes:
+        Inherited from the list class.
+
+    Methods:
+        print_sorted: Print the list in sorted order.
+    """
+
     def print_sorted(self):
-        """prints a sorted list"""
-        print(sorted(self)))
+        """Prints the elements of the list in sorted order.
+
+        This method sorts the list and prints the sorted elements.
+
+        Args:
+            None
+
+        Returns:
+            None
+        """
+        print(sorted(self))
+
--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -1,16 +1,19 @@
 #!/usr/bin/python3
-"""a function that returns True if the object is exactly an instance of the specified class ; otherwise False"""
+
+"""A function that checks if an object is an instance of a specified class."""
 
 def is_same_class(obj, a_class):
     """Check if an object is exactly an instance of a given class.
 
+    This function takes an object and a class as arguments and checks if
+    the object is exactly an instance of the specified class.
+
     Args:
         obj (any): The object to check.
-        a_class (type): The class to match the type of obj
+        a_class (type): The class to match the type of obj.
 
     Returns:
-        true or false
+        bool: True if obj is exactly an instance of a_class, otherwise False.
     """
-     if type(obj) == a_class:
-        return True
-    return False
+    return type(obj) == a_class
+

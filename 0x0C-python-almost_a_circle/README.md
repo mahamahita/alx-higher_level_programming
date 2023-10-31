e509c879ffc7e7f95a6d958fd2b13fc79119abb0
MAHA EL OUARDI <maha600019@gmail.com>
2023-10-31 04:11:27 -0700

    tests

--- 0x0C-python-almost_a_circle/README.md
+++ 0x0C-python-almost_a_circle/README.md
@@ -1,2 +1,1 @@
-circles
-
+Python Almost a circle
--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -1,57 +1,83 @@
 #!/usr/bin/python3
-"""
-square module
-"""
+"""Defines a square class."""
 from models.rectangle import Rectangle
 
 
 class Square(Rectangle):
-    """
-    Square 
-    """
+    """Represent a square."""
+
     def __init__(self, size, x=0, y=0, id=None):
-        """
+        """Initialize a new Square.
+
         Args:
-            size: size of square
-            x: x position 
-            y: y position 
-            id: id of square
+            size (int): The size of the new Square.
+            x (int): The x coordinate of the new Square.
+            y (int): The y coordinate of the new Square.
+            id (int): The identity of the new Square.
         """
         super().__init__(size, size, x, y, id)
 
-    def __str__(self):
-        """
-        overriden method
-        """
-        return "[Square] ({}) {}/{} - {}".format(
-                self.id, self.x, self.y, self.width)
-
     @property
     def size(self):
-        """the size getter"""
+        """Get/set the size of the Square."""
         return self.width
 
     @size.setter
-    def size(self, size):
-        """ the size setter"""
-        self.width = size
-        self.height = size
+    def size(self, value):
+        self.width = value
+        self.height = value
 
     def update(self, *args, **kwargs):
+        """Update the Square.
+
+        Args:
+            *args (ints): New attribute values.
+                - 1st argument represents id attribute
+                - 2nd argument represents size attribute
+                - 3rd argument represents x attribute
+                - 4th argument represents y attribute
+            **kwargs (dict): New key/value pairs of attributes.
         """
-        Assigns attributes
-        """
-        lst = (self.id, self.size, self.x, self.y)
-        if args:
-            self.id, self.size, self.x, self.y = \
-                    args + lst[len(args):len(lst)]
-        elif kwargs:
-            for (key, value) in kwargs.items():
-                setattr(self, key, value)
+        if args and len(args) != 0:
+            a = 0
+            for arg in args:
+                if a == 0:
+                    if arg is None:
+                        self.__init__(self.size, self.x, self.y)
+                    else:
+                        self.id = arg
+                elif a == 1:
+                    self.size = arg
+                elif a == 2:
+                    self.x = arg
+                elif a == 3:
+                    self.y = arg
+                a += 1
+
+        elif kwargs and len(kwargs) != 0:
+            for k, v in kwargs.items():
+                if k == "id":
+                    if v is None:
+                        self.__init__(self.size, self.x, self.y)
+                    else:
+                        self.id = v
+                elif k == "size":
+                    self.size = v
+                elif k == "x":
+                    self.x = v
+                elif k == "y":
+                    self.y = v
 
     def to_dictionary(self):
-        """
-        returns the dictionary representation of a Square
-        """
-        return {'id': self.id, 'x': self.x, 'size': self.size, 'y': self.y}
+        """Return the dictionary representation of the Square."""
+        return {
+            "id": self.id,
+            "size": self.width,
+            "x": self.x,
+            "y": self.y
+        }
 
+    def __str__(self):
+        """Return the print() and str() representation of a Square."""
+        return "[Square] ({}) {}/{} - {}".format(self.id, self.x, self.y,
+                                                 self.width)

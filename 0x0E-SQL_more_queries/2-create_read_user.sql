28d55ba9944ddb2c77b16f737a6bd0a271f532ac
EL OUARDI Maha <maha600019@gmail.com>
2023-11-17 04:56:37 -0800

    tt

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -1,4 +1,4 @@
- -- script that creates the database hbtn_0d_2 and the user user_0d_2
+-- script that creates the database hbtn_0d_2 and the user user_0d_2
 CREATE DATABASE
     IF NOT EXISTS `hbtn_0d_2`;
 CREATE USER
